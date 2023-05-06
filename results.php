1967d991dc9a661f537f61dcd70664598e5fe471
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-05-06 22:24:59 +0000

    linter error fixing

--- results.php
+++ results.php
@@ -9,21 +9,21 @@
 if ((empty($userMin)) || (empty($userMax))) {
       echo "Please enter a minimum and/or a maximum.";
 }
-  
+
  // response for when min is greater than the max
 elseif ($userMin > $userMax) {
       echo "ERROR: You entered a minimum value that is greater than your maximum value. Try a smaller minimum.";
 }
-  
+
  // response for when user enters a valid min and max
 else {
   // starting the while loop to display a list from min to max
-  while ($userMin <= $userMax) {
+    while ($userMin <= $userMax) {
     // build the list by adding the "list" and min with a line break each time
-    $list = $list . $userMin . "<br>";
-    // increment the min by one for each execution of the loop
-    $userMin++;
-  }
+        $list = $list . $userMin . "<br>";
+        // increment the min by one for each execution of the loop
+        $userMin++;
+    }
 
-  echo $list;
+    echo $list;
 }
